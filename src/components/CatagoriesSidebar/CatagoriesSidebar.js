e53085feffe39a15d882d2d6547b91f49434e7a2
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-08-13 00:08:17 +0100

    refactored the code to be so much cleaner with a 80% code reduction

--- src/components/CatagoriesSidebar/CatagoriesSidebar.js
+++ src/components/CatagoriesSidebar/CatagoriesSidebar.js
@@ -5,89 +5,32 @@ import { ACTION } from "../UseReducer/UseReducer";
 import "./CatagoriesSidebar.scss"
 import { CategoriesApi } from "../../CategoriesApi/CategoriesApi";
 const CatagoriesSidebar = () => {
-  let lastItem
-
+  let lastItem=[]
+  let shownTitle
   return ( 
     <aside className="sidebar">
       <div className="catagoriesCont">
         <ul className="catagoriesCont__list">
 
           {CategoriesApi.map((item)=>{
-            console.log(item.title1)
+            console.log(lastItem)
             
-            // if(item.title1!==lastItem){
-            //   lastItem=""
-            // }else{
-            //   lastItem=item.title1
-            // }
+            if(lastItem.includes(item.title1)){
+              shownTitle=""
+
+            }else{
+              shownTitle=item.title1
+              lastItem.push(item.title1)
+            }
           
             return[
-              // <h1>{item.title1}</h1>,
+              <h1 className="catagoriesCont-title">{shownTitle}</h1>,
             <ChangePageButton title={item.title} pageNum={item.page}/>
             ]
           })}
 
         </ul>
       </div>
-
-
-      
-
-          {/* <h2 className="catagoriesCont-title">HTML</h2>
-          <ul className="catagoriesCont__list">
-            <ChangePageButton title={"Tags"} pageNum={0}/>
-            <ChangePageButton title={"Elements"} pageNum={1}/>
-            <ChangePageButton title={"Atrributes"} pageNum={2}/>
-            <ChangePageButton title={"Tips"} pageNum={3}/>
-          </ul>
-
-          <h2 className="catagoriesCont-title">CSS</h2>
-          <ul className="catagoriesCont__list">
-            <ChangePageButton title={"Selectors"} pageNum={4}/>
-            <ChangePageButton title={"Display"} pageNum={5}/>
-            <ChangePageButton title={"Variables"} pageNum={6}/>
-            <ChangePageButton title={"Tips"} pageNum={7}/>
-          </ul>
-
-          <h2 className="catagoriesCont-title">Sass</h2>
-          <ul className="catagoriesCont__list">
-            <ChangePageButton title={"Nesting"} pageNum={8}/>
-            <ChangePageButton title={"Import"} pageNum={9}/>
-            <ChangePageButton title={"Mixin"} pageNum={10}/>
-            <ChangePageButton title={"Variables"} pageNum={11}/>
-            <ChangePageButton title={"Functions"} pageNum={12}/>
-          </ul>
-
-          <h2 className="catagoriesCont-title">JavaScript</h2>
-          <ul className="catagoriesCont__list">
-            <ChangePageButton title={"DataTypes"} pageNum={13}/>
-            <ChangePageButton title={"Classes"} pageNum={14}/>
-            <ChangePageButton title={"Variables"} pageNum={15}/>
-          </ul>
-
-          <h2 className="catagoriesCont-title">REACT</h2>
-          <ul className="catagoriesCont__list">
-            <ChangePageButton title={"DataTypes"} pageNum={16}/>
-            <ChangePageButton title={"Classes"} pageNum={17}/>
-            <ChangePageButton title={"Variables"} pageNum={18}/>
-            <ChangePageButton title={"useEffect"} pageNum={19}/>
-            <ChangePageButton title={"useReducer"} pageNum={20}/>
-            <ChangePageButton title={"useContext"} pageNum={21}/>
-          </ul>
-          <h2 className="catagoriesCont-title">Firebase</h2>
-          <ul className="catagoriesCont__list">
-            <ChangePageButton title={"Auth"} pageNum={22}/>
-            <ChangePageButton title={"Database"} pageNum={23}/>
-            <ChangePageButton title={"Storage"} pageNum={24}/>
-
-
-          </ul>
-          <h2 className="catagoriesCont-title">CodeWars</h2>
-          <ul className="catagoriesCont__list">
-            <ChangePageButton title={"Kata 4"} pageNum={25}/>
-
-          </ul> */}
-      {/* </div> */}
       
     </aside>
    );
