b2c49de1fdc30627d763df156e4c980720ffe3d7
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-08-12 11:54:05 +0100

    gave header items there own components

---
+++ src/components/Header/DropDown/DropDown.js
@@ -0,0 +1,42 @@
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import ChangePageButton from "../../ChangePageButton/ChangePageButton";
+
+const DropDown = () => {
+  return ( 
+    <div className="header__Dropdown">
+        <Link to="./Categories">
+          <p className="header__Dropdown__Categories">Categories</p>
+          <ul className="header__Dropdown__UList">
+            <ol className="header__Dropdown__UList__Dev">
+              <li>Web Dev</li>
+              <ul className="header__Dropdown__UList__Dev__List">
+                <ChangePageButton pageNum={0} title={"HTML"}/>
+                <ChangePageButton pageNum={4} title={"CSS"}/>
+                <ChangePageButton pageNum={8} title={"Sass"}/>
+                <ChangePageButton pageNum={12} title={"JavaScript"}/>
+                <ChangePageButton pageNum={18} title={"React"}/>
+                <ChangePageButton pageNum={22} title={"Firebase"}/>    
+                <ChangePageButton pageNum={25} title={"CodeWars"}/>
+              </ul>
+            </ol>
+
+            <ol className="header__Dropdown__UList__Dev">
+            <li>BackEnd</li>
+            </ol>
+
+            <ol className="header__Dropdown__UList__Dev">
+            <li>ChatRoom</li>
+            </ol>
+
+            <ol className="header__Dropdown__UList__Dev">
+            <li></li>
+            </ol>
+            
+          </ul>
+        </Link> 
+        
+      </div>
+   );
+}
+ 
+export default DropDown;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import { auth } from "../../Lib/FireBase/FireBase";
 import { pageWrapper } from "../App/App";
 import { useContext, useEffect, useState } from "react";
 import SearchCategorie from "../SearchCategorie/SearchCategorie";
-import ChangePageButton from "../ChangePageButton/ChangePageButton";
+import DropDown from "./DropDown/DropDown";
 const Header = () => {
   
   let stateObj = useContext(pageWrapper)
@@ -32,35 +32,7 @@ const Header = () => {
         
       </h1>
       
-      <div className="header__Dropdown">
-        <Link to="./Categories">
-          <p className="header__Dropdown__Categories">Categories</p>
-          <ul className="header__Dropdown__UList">
-            <ol className="header__Dropdown__UList__Dev">
-              <li>Development</li>
-              <ul className="header__Dropdown__UList__Dev__List">
-                <ChangePageButton pageNum={0} title={"HTML"}/>
-                <ChangePageButton pageNum={4} title={"CSS"}/>
-                <ChangePageButton pageNum={8} title={"Sass"}/>
-                <ChangePageButton pageNum={12} title={"JavaScript"}/>
-                <ChangePageButton pageNum={18} title={"React"}/>
-                <ChangePageButton pageNum={22} title={"Firebase"}/>    
-                <ChangePageButton pageNum={25} title={"CodeWars"}/>
-              </ul>
-            </ol>
-            <ol className="header__Dropdown__UList__Dev">
-            <li>ChatRoom</li>
-            </ol>
-            <ol className="header__Dropdown__UList__Dev">
-            <li></li>
-            </ol>
-            <ol className="header__Dropdown__UList__Dev">
-            <li></li>
-            </ol>
-          </ul>
-        </Link> 
-        
-      </div>
+      <DropDown/>
 
       
 
