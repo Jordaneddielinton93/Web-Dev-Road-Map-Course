850a16da06ca84eb998834a2a87497e2a24250b8
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-08-14 23:20:00 +0100

    added more info videos from reliable sources

--- src/CategoriesApi/CategoriesApi.js
+++ src/CategoriesApi/CategoriesApi.js
@@ -11,13 +11,15 @@ export const CategoriesApi=[
     page:1,
     title1:"HTML",
     title:"Elements",
-    discription:"An HTML element is a type of HTML document component, one of several types of HTML nodes. HTML document is composed of a tree of simple HTML nodes, such as text nodes, and HTML elements, which add semantics and formatting to parts of document. Each element can have HTML attributes specified."
+    discription:"An HTML element is a type of HTML document component, one of several types of HTML nodes. HTML document is composed of a tree of simple HTML nodes, such as text nodes, and HTML elements, which add semantics and formatting to parts of document. Each element can have HTML attributes specified.",
+    video:<iframe width="90%" height="472.5" src="https://www.youtube.com/embed/PypMN-yui4Y?start=50" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   },
   {
     page:2,
     title1:"HTML",
     title:"Atrributes",
-    discription:"Attributes define additional characteristics or properties of the element such as width and height of an image. Attributes are always specified in the start tag (or opening tag) and usually consists of name/value pairs like name='value' . Attribute values should always be enclosed in quotation marks."
+    discription:"Attributes define additional characteristics or properties of the element such as width and height of an image. Attributes are always specified in the start tag (or opening tag) and usually consists of name/value pairs like name='value' . Attribute values should always be enclosed in quotation marks.",
+    video:<iframe width="90%" height="472.5" src="https://www.youtube.com/embed/uIWmPL91Dn4?start=12" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   },
   {
     page:3,
@@ -28,19 +30,22 @@ export const CategoriesApi=[
     page:4,
     title1:"CSS",
     title:"Selectors",
-    discription:"In CSS, selectors are used to target the HTML elements on our web pages that we want to style. There are a wide variety of CSS selectors available, allowing for fine-grained precision when selecting elements to style. In this article and its sub-articles we'll run through the different types in great detail, seeing how they work."
+    discription:"In CSS, selectors are used to target the HTML elements on our web pages that we want to style. There are a wide variety of CSS selectors available, allowing for fine-grained precision when selecting elements to style. In this article and its sub-articles we'll run through the different types in great detail, seeing how they work.",
+    video:<iframe width="90%" height="472.5" src="https://www.youtube.com/embed/l1mER1bV0N0?start=12" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   },
   {
     page:5,
     title1:"CSS",
     title:"Display",
-    discription:"The display CSS property sets whether an element is treated as a block or inline element and the layout used for its children, such as flow layout, grid or flex. Formally, the display property sets an element's inner and outer display types."
+    discription:"The display CSS property sets whether an element is treated as a block or inline element and the layout used for its children, such as flow layout, grid or flex. Formally, the display property sets an element's inner and outer display types.",
+    video:<iframe width="90%" height="472.5" src="https://www.youtube.com/embed/Qf-wVa9y9V4?start=12" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   },
   {
     page:6,
     title1:"CSS",
     title:"Variables",
-    discription:"Custom properties (sometimes referred to as CSS variables or cascading variables) are entities defined by CSS authors that contain specific values to be reused throughout a document. Complex websites have very large amounts of CSS, often with a lot of repeated values"
+    discription:"Custom properties (sometimes referred to as CSS variables or cascading variables) are entities defined by CSS authors that contain specific values to be reused throughout a document. Complex websites have very large amounts of CSS, often with a lot of repeated values",
+    video:<iframe width="90%" height="472.5" src="https://www.youtube.com/embed/oZPR_78wCnY?start=12" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   },
   {
     page:7,
@@ -51,29 +56,33 @@ export const CategoriesApi=[
     page:8,
     title1:"SASS",
     title:"Nesting",
-    discription:"Sass wants to make your life easier. Rather than repeating the same selectors over and over again, you can write one style rules inside another. Sass will automatically combine the outer rule’s selector with the inner rule’s."
+    discription:"Sass wants to make your life easier. Rather than repeating the same selectors over and over again, you can write one style rules inside another. Sass will automatically combine the outer rule’s selector with the inner rule’s.",
+    video:<iframe width="90%" height="472.5" src="https://www.youtube.com/embed/8VAcFxGezjM?start=12" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   },
   {
     page:9,
     title1:"SASS",
     title:"Import",
-    discription:"The @import directive allows you to include the content of one file in another. ... sass or . scss file. You can also import CSS files. The @import directive imports the file and any variables or mixins defined in the imported file can then be used in the main file."
+    discription:"The @import directive allows you to include the content of one file in another. ... sass or . scss file. You can also import CSS files. The @import directive imports the file and any variables or mixins defined in the imported file can then be used in the main file.",
+    video:<iframe width="90%" height="472.5" src="https://www.youtube.com/embed/M7HRm0j2ztU?start=12" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   },
   {
     page:10,
     title1:"SASS",
     title:"Mixins",
-    discription:"Mixins allow you to define styles that can be re-used throughout your stylesheet. They make it easy to avoid using non-semantic classes like . ... A mixin's name can be any Sass identifier, and it can contain any statement other than top-level statements."
+    discription:"Mixins allow you to define styles that can be re-used throughout your stylesheet. They make it easy to avoid using non-semantic classes like . ... A mixin's name can be any Sass identifier, and it can contain any statement other than top-level statements.",
+    video:<iframe width="90%" height="472.5" src="https://www.youtube.com/embed/3wXqwX4nu8I?start=12" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   },
   {
     page:11,
     title1:"SASS",
     title:"Variables",
-    discription:"Sass variables are simple: you assign a value to a name that begins with $ , and then you can refer to that name instead of the value itself. ... Unlike a property, which can only be declared in a style rule or at-rule, variables can be declared anywhere you want. To use a variable, just include it in a value."
+    discription:"Sass variables are simple: you assign a value to a name that begins with $ , and then you can refer to that name instead of the value itself. ... Unlike a property, which can only be declared in a style rule or at-rule, variables can be declared anywhere you want. To use a variable, just include it in a value.",
+    video:<iframe width="90%" height="472.5" src="https://www.youtube.com/embed/wLfyzlGqDP4?start=12" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   },
   {
     page:12,
-    title1:"SASS",
+    title1:"JavaScript",
     title:"Functions",
     discription:"Functions allow you to define complex operations on SassScript values that you can re-use throughout your stylesheet. ... A function's name can be any Sass identifier. It can only contain universal statements, as well as the @return at-rule which indicates the value to use as the result of the function call."
   },
